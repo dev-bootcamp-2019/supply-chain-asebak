89281e8c3ab8a610ac48424b8b88d996f8e5b36a
ahmad <radio_frequency@hotmail.com>
2018-11-01 12:57:03 -0400

    added solidity unit tests

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -4,27 +4,61 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
+//referenced from https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+contract ThrowProxy {
+    address public target;
+    bytes data;
+
+    function ThrowProxy(address _target) {
+        target = _target;
+    }
+
+    function() {
+        data = msg.data;
+    }
+
+    function execute() returns (bool) {
+        return target.call(data);
+    }
+}
+
 contract TestSupplyChain {
 
-    // Test for failing conditions in this contracts
-    // test that every modifier is working
 
-    // buyItem
+    uint public initialBalance = 10 ether;
 
-    // test for failure if user does not send enough funds
-    // test for purchasing an item that is not for Sale
+    function testItemNotForSale() {
+        SupplyChain supplyChainContract = new SupplyChain();
+        ThrowProxy throwProxy = new ThrowProxy(address(supplyChainContract));
+        SupplyChain(address(throwProxy)).addItem("test1", 15 ether);
+        bool r1 = throwProxy.execute.gas(200000)();
+        Assert.isTrue(r1, "Transaction adding items should not throw errors");
+        SupplyChain(address(throwProxy)).buyItem(6433);
+        bool r2 = throwProxy.execute.gas(200000)();
+        Assert.isFalse(r2, "forSale modifier should be thrown");
+    }
 
+    function testShipItemNotSold() {
+        SupplyChain supplyChainContract = new SupplyChain();
+        ThrowProxy throwProxy = new ThrowProxy(address(supplyChainContract));
+        SupplyChain(address(throwProxy)).addItem("test1", 15 ether);
+        bool r1 = throwProxy.execute.gas(200000)();
+        Assert.isTrue(r1, "Transaction adding items should not throw errors");
+        SupplyChain(address(throwProxy)).shipItem(0);
+        bool r2 = throwProxy.execute.gas(200000)();
+        Assert.isFalse(r2, "sold modifier should be thrown");
+    }
 
-    // shipItem
-
-    // test for calls that are made by not the seller
-    // test for trying to ship an item that is not marked Sold
-
-    // receiveItem
-
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
-
+    function testReceivedItemNotShipped() {
+        SupplyChain supplyChainContract = new SupplyChain();
+        ThrowProxy throwProxy = new ThrowProxy(address(supplyChainContract));
+        SupplyChain(address(throwProxy)).addItem("test1", 15 ether);
+        bool r1 = throwProxy.execute.gas(200000)();
+        Assert.isTrue(r1, "Transaction adding items should not throw errors");
+        SupplyChain(address(throwProxy)).receiveItem(0);
+        bool r2 = throwProxy.execute.gas(200000)();
+        Assert.isFalse(r2, "shipped modifier should be thrown");
+    }
      
 
 
